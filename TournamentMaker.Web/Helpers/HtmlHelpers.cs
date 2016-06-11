299132e3d4807ee3e28388bdf628389a6941bb13
Phil Haack <haacked@gmail.com>
2016-06-11 11:11:57 -0700

    Improve display of tournaments

---
+++ TournamentMaker.Web/Helpers/HtmlHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+public static class HtmlHelpers
+{
+    /// <summary>
+    /// Replaces new lines with BR tags but ensures that the rest of the content is encoded.
+    /// </summary>
+    /// <param name="helper"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static HtmlString Multiline<T>(this HtmlHelper<T> helper, string value)
+    {
+        return new HtmlString(helper.Encode(value).Replace(Environment.NewLine, "<br />"));
+    }
+}
